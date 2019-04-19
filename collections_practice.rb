a6cbbdb824492ea967efe80e40c5bcb91acb6155
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-19 05:50:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1],array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|x| x[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  sum = 0 
+  array.each {|num| sum += num}
+  sum
+end
+
+def add_s(array)
+  array.map do |w|
+    if w == array[1]
+      w
+    else 
+      w + "s"
+    end
+  end
+end
