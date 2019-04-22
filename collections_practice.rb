59fb572819eb7d7f255282f5ffb1f81f9995ec46
Lorena C <lorena@eunknown.org>
2019-04-22 00:05:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
+sort_array_asc([25, 7, 1])
 
+def sort_array_desc(desc)
+  desc.sort{| a , b | b <=> a}
+end
+
+def sort_array_char_count(word_ct)
+  word_ct.sort{|short, long| short.length <=> long.length}
+end
+
+def swap_elements(switcho)
+  switcho[1], switcho[2] = switcho[2], switcho[1]
+  switcho
+end
+
+def reverse_array(reverso)
+  reverso.reverse
+end
+
+def kesha_maker(mula)
+  mula.each {|casho|casho[2] = "$"}
+end
+
+def find_a(finder)
+  finder.find_all.each{|vowel|vowel[0]=="a"}
+end
+
+def sum_array(adder)
+  adder.inject(0){|sum,x| sum + x}
+end
+
+def add_s(feets)
+  feets.each_with_index.collect each{|word,location|}
+end
